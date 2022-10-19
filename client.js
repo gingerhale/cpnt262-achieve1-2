ab7d9595dd3517e40358a9b6784ff496aa2411ef
gingerhale <haleydickson5@gmail.com>
2022-10-19 11:44:10 -0600

    added comments to js file

--- client.js
+++ client.js
@@ -3,10 +3,12 @@ const myChart = document.getElementById('myChart').getContext('2d');
     const massPopChart = new Chart(myChart, {
       type:'bar', 
       data:{
+        // this below changed the title of each bar on the graft
         labels:['Calgary', 'Edmonton', 'Red Deer', 'Lethbridge', 'Airdrie', 'Fort McMurray'],
         datasets:[{
           label:'Population',
           data:[
+          // this below changes the number value of each bar on the graph
           1305550,
           1151635,
           99846,
@@ -14,6 +16,8 @@ const myChart = document.getElementById('myChart').getContext('2d');
           73578,
           68002
           ],
+
+          // this below changed the color of all the bars on the graph
           backgroundColor:[
             '#770087',
             '#52009E',
